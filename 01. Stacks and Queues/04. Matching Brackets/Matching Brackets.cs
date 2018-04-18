4ff911e3ebe4f927a16630059023c1d258e1742b
pirocorp <pirocorp@abv.bg>
2018-04-18 13:29:33 +0200

    04. Matching Brackets

---
+++ 01. Stacks and Queues/04. Matching Brackets/Matching Brackets.cs
@@ -0,0 +1,41 @@
+﻿namespace _04.Matching_Brackets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MatchingBrackets
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var indexesOfOpeningBrackets = new Stack<int>();
+            var results = new List<string>();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var currentChar = input[i];
+
+                if (currentChar == '(')
+                {
+                    indexesOfOpeningBrackets.Push(i);
+                }
+
+                if (currentChar == ')')
+                {
+                    var startIndex = indexesOfOpeningBrackets.Pop();
+                    var endIndex = i;
+
+                    var lenghtOfSubstring = (endIndex - startIndex) + 1;
+                    var currentSubstring = input.Substring(startIndex, lenghtOfSubstring);
+
+                    results.Add(currentSubstring);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, results));
+        }
+    }
+}
