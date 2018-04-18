f11de698b0e519f8f640e0c435ed56594ae36139
pirocorp <pirocorp@abv.bg>
2018-04-18 13:43:16 +0200

    05. Hot Potato

---
+++ 01. Stacks and Queues/05. Hot Potato/Hot Potato.cs
@@ -0,0 +1,30 @@
+﻿namespace _05.Hot_Potato
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class HotPotato
+    {
+        public static void Main()
+        {
+            var childrens = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            var number = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>(childrens);
+
+            while (queue.Count > 1)
+            {
+                for (var i = 1; i < number; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
--- 01. Stacks and Queues/05. Hot Potato/Properties/AssemblyInfo.cs
+++ 01. Stacks and Queues/05. Hot Potato/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01.Reverse Strings")]
+[assembly: AssemblyTitle("05. Hot Potato")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01.Reverse Strings")]
+[assembly: AssemblyProduct("05. Hot Potato")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4a3b6eda-f54f-4c8d-b96a-9b575a7d042a")]
+[assembly: Guid("56b9acb1-0eaa-4479-bda6-be80e00dc091")]
 
 // Version information for an assembly consists of the following four values:
 //
