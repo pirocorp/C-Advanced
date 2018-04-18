a70de860fe7ae1a1c92f5235d15154e5c749876c
pirocorp <pirocorp@abv.bg>
2018-04-18 14:07:05 +0200

    More file management

---
+++ 01. Stacks and Queues/01. Stacks-And-Queues-Exercises/01. Reverse Numbers/Reverse Numbers.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.Reverse_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ReverseNumbers
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
