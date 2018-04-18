b1d7580bbd1e1011dc2042570b76f8cd43dcca7c
pirocorp <pirocorp@abv.bg>
2018-04-18 12:39:57 +0200

    01. Reverse Strings

---
+++ 01. Stacks and Queues/01.Reverse Strings/Reverse Strings.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Reverse_Strings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ReverseStrings
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var charStack = new Stack<char>();
+
+            foreach (var currentChar in input)
+            {
+                charStack.Push(currentChar);
+            }
+
+            Console.WriteLine(string.Join("", charStack));
+        }
+    }
+}
