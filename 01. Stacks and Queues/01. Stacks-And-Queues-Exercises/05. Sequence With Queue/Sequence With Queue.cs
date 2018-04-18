9d350db43a22223639e5122c23386186a4ca6d08
pirocorp <pirocorp@abv.bg>
2018-04-18 21:14:51 +0200

    05. Sequence With Queue

---
+++ 01. Stacks and Queues/01. Stacks-And-Queues-Exercises/05. Sequence With Queue/Sequence With Queue.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.Sequence_With_Queue
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SequenceWithQueue
+    {
+        public static void Main()
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var queueGenerated = new Queue<long>();
+            var queueFinal = new Queue<long>();
+
+            var sum = 0;
+
+            var elementX = n;
+            queueGenerated.Enqueue(elementX);
+
+            while (queueGenerated.Count < 50)
+            {
+                var generatedElementG1 = elementX + 1;
+                var generatedElementG2 = 2 * elementX + 1;
+                var generatedElementG3 = elementX + 2;
+
+                queueGenerated.Enqueue(generatedElementG1);
+                queueGenerated.Enqueue(generatedElementG2);
+                queueGenerated.Enqueue(generatedElementG3);
+
+                queueFinal.Enqueue(queueGenerated.Dequeue());
+                elementX = queueGenerated.Peek();
+            }
+
+            //Console.WriteLine($"Generated Count: {queueGenerated.Count}");
+            //Console.WriteLine($"Final Count: {queueFinal.Count}");
+
+            for (var i = queueFinal.Count; i < 50; i++)
+            {
+                queueFinal.Enqueue(queueGenerated.Dequeue());
+            }
+            
+            Console.WriteLine(string.Join(" ", queueFinal));
+        }
+    }
+}
