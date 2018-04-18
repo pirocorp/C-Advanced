3d9c8f36f50c4be110c95e9a91f69d961c416e01
pirocorp <pirocorp@abv.bg>
2018-04-18 13:05:20 +0200

    02. Simple Calculator

---
+++ 01. Stacks and Queues/02. Simple Calculator/Simple Calculator.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.Simple_Calculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Reverse();
+
+            var stringStack = new Stack<string>(input);
+
+            while (stringStack.Count > 1)
+            {
+                var first = int.Parse(stringStack.Pop());
+                var op = stringStack.Pop();
+                var second = int.Parse(stringStack.Pop());
+
+                switch (op)
+                {
+                    case "+":
+                        stringStack.Push((first + second).ToString());
+                        break;
+                    case "-":
+                        stringStack.Push((first - second).ToString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(stringStack.Pop());
+        }
+    }
+}
