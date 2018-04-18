3187c84c3c9f941933db4cbfb004d94eab0c702d
pirocorp <pirocorp@abv.bg>
2018-04-18 13:18:21 +0200

    03. Decimal to Binary Converter

---
+++ 01. Stacks and Queues/03. Decimal to Binary Converter/Decimal to Binary Converter.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.Decimal_to_Binary_Converter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DecimalToBinaryConverter
+    {
+        public static void Main()
+        {
+            var decimalNumber = int.Parse(Console.ReadLine());
+            var binDigits = new Stack<byte>();
+
+            if (decimalNumber == 0)
+            {
+                binDigits.Push(0);
+            }
+
+            while (decimalNumber != 0)
+            {
+                binDigits.Push((byte)(decimalNumber % 2));
+                decimalNumber /= 2;
+            }
+
+            Console.WriteLine(string.Join("", binDigits));
+        }
+    }
+}
