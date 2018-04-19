b597db7868afaf24391cd4a164cb3659354d0158
pirocorp <pirocorp@abv.bg>
2018-04-19 10:44:03 +0200

    07. Balanced Parenthesis

---
+++ 01. Stacks and Queues/01. Stacks-And-Queues-Exercises/07. Balanced Parenthesis/Balanced Parenthesis.cs
@@ -0,0 +1,71 @@
+﻿namespace _07.Balanced_Parenthesis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BalancedParenthesis
+    {
+        public static void Main()
+        {
+            var inputParenthesis = Console.ReadLine().ToCharArray();
+            var stackOfParenthesis = new Stack<char>();
+
+            foreach (var item in inputParenthesis)
+            {
+                if (item == '{' || item == '[' || item == '(')
+                {
+                    stackOfParenthesis.Push(item);
+                    continue;
+                }
+
+                if (stackOfParenthesis.Count == 0|| !IsClosingParenthesisToCurrentParenthesis(stackOfParenthesis.Peek(), item))
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+
+                stackOfParenthesis.Pop();
+            }
+
+            if (stackOfParenthesis.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        private static bool IsClosingParenthesisToCurrentParenthesis(char openParenthesis, char closingParenthesis)
+        {
+            switch (openParenthesis)
+            {
+                case '{':
+                    if (closingParenthesis == '}')
+                    {
+                        return true;
+                    }
+                    break;
+                case '[':
+                    if (closingParenthesis == ']')
+                    {
+                        return true;
+                    }
+                    break;
+                case '(':
+                    if (closingParenthesis == ')')
+                    {
+                        return true;
+                    }
+                    break;
+                default: return false;
+            }
+
+            return false;
+        }
+    }
+}
