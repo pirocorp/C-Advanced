8e97a55d94b7f16c11ed9d41a9ea66cc4d9e978d
pirocorp <pirocorp@abv.bg>
2018-04-20 15:32:29 +0200

    01. Matrix of Palindromes

---
+++ 02. Multidimensional Arrays/02. Multidimensional-Arrays-Exercise/01. Matrix of Palindromes/Matrix of Palindromes.cs
@@ -0,0 +1,42 @@
+﻿namespace _01.Matrix_of_Palindromes
+{
+    using System;
+    using System.Linq;
+
+    public class MatrixOfPalindromes
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            var rows = numbers[0];
+            var cols = numbers[1];
+
+            var matrix = new string[rows][];
+
+            for (var rowIndex = 0; rowIndex < rows; rowIndex++)
+            {
+                matrix[rowIndex] = new string[cols];
+
+                matrix[rowIndex][0] = new string(alphabet[rowIndex], 3);
+                matrix[rowIndex][cols - 1] = $"{alphabet[rowIndex]}{alphabet[(cols - 1 ) + rowIndex]}{alphabet[rowIndex]}";
+
+                for (var colIndex = 1; colIndex < cols - 1; colIndex++)
+                {
+                    matrix[rowIndex][colIndex] =
+                        $"{alphabet[rowIndex]}{alphabet[rowIndex + colIndex]}{alphabet[rowIndex]}";
+                }
+            }
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
