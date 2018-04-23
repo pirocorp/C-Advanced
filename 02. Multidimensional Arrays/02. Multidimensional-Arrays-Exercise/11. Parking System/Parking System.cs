6d888bcf97dabf234ad5aaa1df296fec1c6fb1ab
pirocorp <pirocorp@abv.bg>
2018-04-23 19:19:37 +0200

    This solution get 80/100 points in Judge. Tests 5 & 9 break the memory limit!

---
+++ 02. Multidimensional Arrays/02. Multidimensional-Arrays-Exercise/11. Parking System/Parking System.cs
@@ -0,0 +1,106 @@
+﻿namespace _11.Parking_System
+{
+    using System;
+    using System.Linq;
+
+    public class ParkingSystem
+    {
+        public static void Main()
+        {
+            var dimensions = Console.ReadLine()
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = dimensions[0];
+            var cols = dimensions[1];
+
+            var parkingLot = InitializeParkingLot(rows, cols);
+
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "stop")
+            {
+                var tokens = inputLine
+                    .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var entryRow = tokens[0];
+
+                var row = tokens[1];
+                var col = tokens[2];
+
+                var distance = CalculateDistance(parkingLot, entryRow, row, col);
+
+                if (distance == -1)
+                {
+                    Console.WriteLine($"Row {row} full");
+                }
+                else
+                {
+                    Console.WriteLine(distance);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+
+        private static int CalculateDistance(bool[][] parkingLot, int entryRow, int destinationRow, int destinationCol)
+        {
+            var distance = Math.Abs(destinationRow - entryRow);
+
+            if (parkingLot[destinationRow][destinationCol])
+            {
+                destinationCol = FindClosestFreeSpot(destinationCol, parkingLot[destinationRow]);
+            }
+
+            if (destinationCol == -1)
+            {
+                return -1;
+            }
+
+            parkingLot[destinationRow][destinationCol] = true;
+
+            return distance + (destinationCol + 1);
+        }
+
+        private static int FindClosestFreeSpot(int destinationCol, bool[] parkingSpaces)
+        {
+            if (parkingSpaces.Any(b => b == false))
+            {
+                var lenght = Math.Max(destinationCol, parkingSpaces.Length - destinationCol);
+
+                for (var i = 1; i < lenght; i++)
+                {
+                    var lowerIndex = Math.Max(1, destinationCol - i);
+                    var upperIndex = Math.Min(parkingSpaces.Length - 1, destinationCol + i);
+
+                    if (parkingSpaces[lowerIndex] == false)
+                    {
+                        return lowerIndex;
+                    }
+
+                    if (parkingSpaces[upperIndex] == false)
+                    {
+                        return upperIndex;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static bool[][] InitializeParkingLot(int rows, int cols)
+        {
+            var parkingLot = new bool[rows][];
+
+            for (var rowIndex = 0; rowIndex < rows; rowIndex++)
+            {
+                parkingLot[rowIndex] = new bool [cols];
+            }
+
+            return parkingLot;
+        }
+    }
+}
