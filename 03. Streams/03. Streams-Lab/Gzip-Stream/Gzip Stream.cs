8dcb2d882e5873cd6cb83d3db3d925d976fce19d
pirocorp <pirocorp@abv.bg>
2018-04-24 22:31:29 +0200

    08. Gzip Stream

---
+++ 03. Streams/03. Streams-Lab/Gzip-Stream/Gzip Stream.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+public class GzipStreamDemo
+{
+    public static void Main()
+    {
+        Compress("../../text.txt", "../../zipped.gz");
+
+        Decompress("../../zipped.gz", "../../result.txt");
+    }
+
+    public static void Decompress(string inputFile, string outputFile)
+    {
+        using (var inputStream = new FileStream(inputFile, FileMode.Open))
+        {
+            using (var compressionStream = new GZipStream(inputStream, CompressionMode.Decompress, false))
+            {
+                using (var outputStream = new FileStream(outputFile, FileMode.Create))
+                {
+                    var buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        var readBytes = compressionStream.Read(buffer, 0, buffer.Length);
+
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+
+                        outputStream.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void Compress(string inputFile, string outputFile)
+    {
+        using (var inputStream = new FileStream(inputFile, FileMode.Open))
+        {
+            using (var outputStream = new FileStream(outputFile, FileMode.Create))
+            {
+                using (var compressionStream = new GZipStream(outputStream, CompressionMode.Compress, false))
+                {
+                    var buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        var readBytes = inputStream.Read(buffer, 0, buffer.Length);
+
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+
+                        compressionStream.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+}
