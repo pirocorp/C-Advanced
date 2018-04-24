59ba7ff17e3a3f5937f522b145bfc5d894955ac4
pirocorp <pirocorp@abv.bg>
2018-04-24 22:37:16 +0200

    09. Fixing Movie Subtitles

---
+++ 03. Streams/03. Streams-Lab/Fixing Movie Subtitles/FixingMovieSubtitles.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+public class FixingMovieSubtitles
+{
+    private const int Addition = 5000;
+    private const string InputFile = @"..\..\source.sub";
+    private const string OutputFile = @"..\..\fixed.sub";
+    private const string SubtitlePattern = "{(.+)}{(.+)} (.+)";
+
+    public static void Main()
+    {
+        var streamReader = new StreamReader(InputFile);
+        var streamWriter = new StreamWriter(OutputFile);
+
+        try
+        {
+            var regex = new Regex(SubtitlePattern);
+            string line;
+
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                var match = regex.Match(line);
+                var startTime = int.Parse(match.Groups[1].Value) + Addition;
+                var endTime = int.Parse(match.Groups[2].Value) + Addition;
+                var text = match.Groups[3].Value;
+
+                streamWriter.WriteLine("{{{0}}}{{{1}}} {2}", startTime, endTime, text);
+            }
+        }
+        finally
+        {
+            streamReader.Close();
+            streamWriter.Close();
+        }
+    }
+}
