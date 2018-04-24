2d0d02ccde64961824d94d4e4bcfce054c085534
pirocorp <pirocorp@abv.bg>
2018-04-24 21:37:18 +0200

    01. Stream Reader

---
+++ 03. Streams/03. Streams-Lab/Stream-Reader/StreamReaderDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+public class StreamReaderDemo
+{
+    public static void Main(string[] args)
+    {
+        using (StreamReader reader = new StreamReader("../../somefile.txt"))
+        {
+            var lineNumber = 0;
+            var line = reader.ReadLine();
+
+            while (line != null)
+            {
+                lineNumber++;
+                Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
+    
