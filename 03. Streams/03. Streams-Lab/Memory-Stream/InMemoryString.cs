fbf81d53625148f79d481cc281bb0b7140039341
pirocorp <pirocorp@abv.bg>
2018-04-24 22:00:32 +0200

    05. Memory Stream

---
+++ 03. Streams/03. Streams-Lab/Memory-Stream/InMemoryString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+public class InMemoryString
+{
+    public static void Main()
+    {
+        var text = "Two households, both alike in dignity,In fair Verona, where we lay our scene";
+        var bytes = Encoding.UTF8.GetBytes(text);
+
+        using (var memoryStream = new MemoryStream(bytes))
+        {
+            while (true)
+            {
+                var readByte = memoryStream.ReadByte();
+                if (readByte == -1)
+                {
+                    break;                    
+                }
+
+                Console.WriteLine(readByte.ToString("X"));
+            }
+        }
+    }
+}
