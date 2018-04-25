cf26cbab434e50ad62d903bfbbfd6eb2a713a85f
pirocorp <pirocorp@abv.bg>
2018-04-25 10:35:06 +0200

    01. Odd Lines

---
+++ 03. Streams/03. Streams-Exercise/01. Odd Lines/Odd Lines.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.Odd_Lines
+{
+    using System;
+    using System.IO;
+
+
+    public class OddLines
+    {
+        public static void Main()
+        {
+            using (var streamReader = new StreamReader("../../text.txt"))
+            {
+                var lineNumber = 0;
+                var line = streamReader.ReadLine();
+
+                while (line != null)
+                {
+                    if (lineNumber % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    line = streamReader.ReadLine();
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
