9c1cc1138b064368b2aaf12ba1d2cba11ab03eda
pirocorp <pirocorp@abv.bg>
2018-04-25 16:32:44 +0200

    08. Full Directory Traversal

---
+++ 03. Streams/03. Streams-Exercise/08. Full Directory Traversal/Full Directory Traversal.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+
+namespace _08.Full_Directory_Traversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FullDirectoryTraversal
+    {
+        public static void Main()
+        {
+            var startPath = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+
+            PrintAllFiles(startPath);
+        }
+
+        private static void PrintAllFiles(string path)
+        {
+            var directories = Directory.GetDirectories(path)
+                .Where(x => !x.Contains("$"))
+                .Where(x => !x.Contains("System Volume Information"));
+            var files = Directory.GetFiles(path);
+
+            //Console.Clear();
+            //Console.SetCursorPosition(0, 0);
+            Console.WriteLine(path);
+
+            AppendAllFilesPerDirectoryToReportFile(files);
+
+            foreach (var directory in directories)
+            {
+                PrintAllFiles(directory);
+            }
+        }
+
+        private static void AppendAllFilesPerDirectoryToReportFile(string[] files)
+        {
+            var filesPerExtension = new Dictionary<string, List<string>>();
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                var currentFile = files[i];
+                var index = currentFile.IndexOf(".");
+
+                var currentExtension = string.Empty;
+
+                if (index >= 0)
+                {
+                    currentExtension = currentFile.Substring(index, currentFile.Length - index);
+                }
+
+                if (!filesPerExtension.ContainsKey(currentExtension))
+                {
+                    filesPerExtension[currentExtension] = new List<string>();
+                }
+
+                filesPerExtension[currentExtension].Add(currentFile);
+            }
+
+            filesPerExtension = filesPerExtension
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            desktopPath = $"{desktopPath}\\report.txt";
+
+            foreach (var extensionFiles in filesPerExtension)
+            {
+                var extension = extensionFiles.Key;
+
+                var filesList = extensionFiles
+                    .Value.Select(x =>
+                    {
+                        var fileInfo = new FileInfo(x);
+                        var size = fileInfo.Length;
+                        return new KeyValuePair<string, long>(x, size);
+                    })
+                    .OrderByDescending(x => x.Value)
+                    .Select(x => $"--{x.Key} - {x.Value / 1024M:F3}kb")
+                    .ToArray();
+
+                File.AppendAllText(desktopPath, $"{extension}{Environment.NewLine}");
+                File.AppendAllLines(desktopPath, filesList);
+            }
+        }
+    }
+}
