e14538a2d8d643f339cde734e25cd66540fe6180
pirocorp <pirocorp@abv.bg>
2018-04-25 10:44:10 +0200

    02. Line Numbers

---
+++ 03. Streams/03. Streams-Exercise/02. Line Numbers/Line Numbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.Line_Numbers
+{
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var streamReader = new StreamReader("../../text.txt"))
+            {
+                using (var streamWrither = new StreamWriter("../../output.txt"))
+                {
+                    var line = string.Empty;
+                    var lineNumber = 1;
+
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        streamWrither.WriteLine($"Line {lineNumber++}: {line}");
+                    }
+                }
+            }
+        }
+    }
+}
