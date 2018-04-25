53c37bfc423ba0152eafc3e9f44647b1a040cc81
pirocorp <pirocorp@abv.bg>
2018-04-25 12:25:05 +0200

    04. Copy Binary File

---
+++ 03. Streams/03. Streams-Exercise/04. Copy Binary File/Copy Binary File.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.Copy_Binary_File
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        public static void Main()
+        {
+            using (var readStream = new FileStream("../../copyMe.png", FileMode.Open))
+            {
+                using (var writeStream = new FileStream("../../output.png", FileMode.Create))
+                {
+                    var fileLength = readStream.Length;
+                    var buffer = new byte[4096];
+
+                    var readBytes = -1;
+
+                    while ((readBytes = readStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        writeStream.Write(buffer, 0, readBytes);
+
+                        Console.WriteLine($"{Math.Min(readStream.Position / (double)fileLength, 1):P}");
+                    }
+                }
+            }
+        }
+    }
+} 
