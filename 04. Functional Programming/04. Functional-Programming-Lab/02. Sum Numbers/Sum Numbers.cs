a5747ac3b6f5311175f88c71a6cc2337f02b5ab1
pirocorp <pirocorp@abv.bg>
2018-04-26 16:19:29 +0200

    02. Sum Numbers

---
+++ 04. Functional Programming/04. Functional-Programming-Lab/02. Sum Numbers/Sum Numbers.cs
@@ -0,0 +1,21 @@
+﻿namespace _02._Sum_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class SumNumbers
+    {
+        public static void Main()
+        {
+            Func<string, int> parser = n => int.Parse(n);
+
+            var numbers = Console.ReadLine()
+                .Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
