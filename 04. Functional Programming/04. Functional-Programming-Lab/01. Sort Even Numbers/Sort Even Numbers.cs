b6bfdec68b3c4132a435647250a3d82c7913304d
pirocorp <pirocorp@abv.bg>
2018-04-26 16:13:55 +0200

    01. Sort Even Numbers

---
+++ 04. Functional Programming/04. Functional-Programming-Lab/01. Sort Even Numbers/Sort Even Numbers.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Sort_Even_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class SortEvenNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n);
+
+            var result = string.Join(", ", numbers);
+
+            Console.WriteLine(result);
+        }
+    }
+}
