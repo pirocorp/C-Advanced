2cabe5c559eb9ac3baa7cb73bbe0173c6c33ea00
pirocorp <pirocorp@abv.bg>
2018-04-26 19:20:57 +0200

    02. Knights of Honor

---
+++ 04. Functional Programming/04. Functional-Programming-Exercises/02. Knights of Honor/Knights of Honor.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Knights_of_Honor
+{
+    using System;
+    using System.Linq;
+
+    public class KnightsOfHonor
+    {
+        public static void Main(string[] args)
+        {
+            Action<string> printName = s => Console.WriteLine($"Sir {s}");
+
+            Console.ReadLine()
+                .Split(new[] {" ", "\t"}, StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(printName);
+        }
+    }
+}
