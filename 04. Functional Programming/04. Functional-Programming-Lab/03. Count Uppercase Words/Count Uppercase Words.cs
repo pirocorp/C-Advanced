527a0437b906705eafd6010d679ae43a2f8b3eba
pirocorp <pirocorp@abv.bg>
2018-04-26 16:28:33 +0200

    03. Count Uppercase Words

---
+++ 04. Functional Programming/04. Functional-Programming-Lab/03. Count Uppercase Words/Count Uppercase Words.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Count_Uppercase_Words
+{
+    using System;
+    using System.Linq;
+
+    public class CountUppercaseWords
+    {
+        public static void Main()
+        {
+            var wordsUpperCase = Console.ReadLine()
+                .Split(new[] {" ", ", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => char.IsUpper(w[0]))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, wordsUpperCase));
+        }
+    }
+}
