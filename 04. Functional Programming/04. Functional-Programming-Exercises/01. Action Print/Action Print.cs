962a103adf6ce3d08825ba24b2de58159beab58f
pirocorp <pirocorp@abv.bg>
2018-04-26 17:22:12 +0200

    01. Action Print

---
+++ 04. Functional Programming/04. Functional-Programming-Exercises/01. Action Print/Action Print.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Action_Print
+{
+    using System;
+
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            var stringsList = Console.ReadLine().Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = x => Console.WriteLine(x);
+
+            foreach (var str in stringsList)
+            {
+                print(str);
+            }
+        }
+    }
+}
