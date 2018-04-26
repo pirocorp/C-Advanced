f716a4cf257a3578d196e9eda77a3172be1881a0
pirocorp <pirocorp@abv.bg>
2018-04-26 19:36:53 +0200

    03. Custom Min Function

---
+++ 04. Functional Programming/04. Functional-Programming-Exercises/03. Custom Min Function/Custom Min Function.cs
@@ -0,0 +1,35 @@
+﻿namespace _03._Custom_Min_Function
+{
+    using System;
+    using System.Linq;
+
+    public class CustomMinFunction
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> minElement = GetMin;
+
+            Console.WriteLine(minElement(numbers));
+        }
+
+        private static int GetMin(int[] numbers)
+        {
+            var min = numbers[0];
+
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
