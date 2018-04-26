92e1417244d56f2a105c0739e107611beeb368ce
pirocorp <pirocorp@abv.bg>
2018-04-26 20:26:54 +0200

    07. Predicate For Names

---
+++ 04. Functional Programming/04. Functional-Programming-Exercises/07. Predicate For Names/Predicate For Names.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._Predicate_For_Names
+{
+    using System;
+    using System.Linq;
+
+    public class PredicateForNames  
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Func<string, bool> filter = x => x.Length <= n;
+
+            Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => filter(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
