75034ab66beb0dcb7bb66a9404f5e8c9af79cacf
pirocorp <pirocorp@abv.bg>
2018-04-26 20:19:08 +0200

    06. Reverse And Exclude

---
+++ 04. Functional Programming/04. Functional-Programming-Exercises/06. Reverse And Exclude/Reverse And Exclude.cs
@@ -0,0 +1,23 @@
+﻿namespace _06._Reverse_And_Exclude
+{
+    using System.Linq;
+    using System;
+
+    public class ReverseAndExclude
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var divisor = int.Parse(Console.ReadLine());
+
+            Func<int, bool> operation = x => x % divisor != 0;
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => operation(x)).Reverse()));
+
+        }
+    }
+}
