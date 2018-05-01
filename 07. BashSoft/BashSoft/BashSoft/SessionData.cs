090f928925a51c23651d985bff92b7b9c39c3da1
pirocorp <pirocorp@abv.bg>
2018-05-01 13:25:55 +0200

    Problem 8. Saving some data for our current session

--- 07. BashSoft/BashSoft/BashSoft/SessionData.cs
+++ 07. BashSoft/BashSoft/BashSoft/SessionData.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BashSoft
+﻿namespace BashSoft
 {
-    class SessionData
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
+
+    public static class SessionData
     {
+        public static string currentPath = Directory.GetCurrentDirectory();
     }
 }
