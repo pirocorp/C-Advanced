f864f07a213eadee0d7b5a0566be7608ec022305
pirocorp <pirocorp@abv.bg>
2018-05-02 12:08:02 +0200

    Part VII. LINQ Problem 24. Change Predicate Methods with Lambda Expressions

--- 07. BashSoft/BashSoft/BashSoft/RepositoryFilters.cs
+++ 07. BashSoft/BashSoft/BashSoft/RepositoryFilters.cs
@@ -12,15 +12,15 @@
 
             if (wantedFilter == "excellent")
             {
-                FilterAndTake(wantedData, ExcellentFilter, studentsToTake);
+                FilterAndTake(wantedData, x => x >= 5, studentsToTake);
             }
             else if (wantedFilter == "average")
             {
-                FilterAndTake(wantedData, AverageFilter, studentsToTake);
+                FilterAndTake(wantedData, x => x < 5 && x > 3.5, studentsToTake);
             }
             else if (wantedFilter == "poor")
             {
-                FilterAndTake(wantedData, PoorFilter, studentsToTake);
+                FilterAndTake(wantedData, x => x < 3.5, studentsToTake);
             }
             else
             {
@@ -48,21 +48,6 @@
                     counterForPrinted++;
                 }
             }
-        }
-
-        private static bool ExcellentFilter(double mark)
-        {
-            return mark >= 5.0;
-        }
-
-        private static bool AverageFilter(double mark)
-        {
-            return mark < 5.0 && mark >= 3.5;
-        }
-
-        private static bool PoorFilter(double mark)
-        {
-            return mark < 3.5;
         }
 
         private static double Average(List<int> scoresOnTasks)
