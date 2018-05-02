aeb0d8e72fc78319c1de8a9c262e97b561ad8fce
pirocorp <pirocorp@abv.bg>
2018-05-02 10:55:37 +0200

    Implement Private OrderAndTake Method Implement Private GetSortedStudents

--- 07. BashSoft/BashSoft/BashSoft/RepositorySorters.cs
+++ 07. BashSoft/BashSoft/BashSoft/RepositorySorters.cs
@@ -14,7 +14,12 @@
         private static void OrderAndTake(Dictionary<string, List<int>> wantedData, int studentToTake,
             Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
         {
+            var studentsSorted = GetSortedStudents(wantedData, studentToTake, comparisonFunc);
 
+            foreach (var student in studentsSorted)
+            {
+                OutputWriter.DisplayStudent(student);
+            }
         }
 
         private static int CompareInOrder(KeyValuePair<string, List<int>> firstValue,
@@ -55,6 +60,53 @@
             }
 
             return totalScoreOfFirst.CompareTo(totalScoreOfSecond);
+        }
+
+        private static Dictionary<string, List<int>> GetSortedStudents(Dictionary<string, List<int>> wantedData,
+            int studentToTake,
+            Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
+        {
+            var studentsTaken = 0;
+            var studentsSorted = new Dictionary<string, List<int>>();
+            var nextInOrder = new KeyValuePair<string, List<int>>();
+
+            var isSorted = false;
+
+            while (studentsTaken < studentToTake)
+            {
+                isSorted = true;
+
+                foreach (var student_score in wantedData)
+                {
+                    if (!string.IsNullOrEmpty(nextInOrder.Key))
+                    {
+                        var comparisonResult = comparisonFunc(student_score, nextInOrder);
+
+                        if (comparisonResult >= 0 && !studentsSorted.ContainsKey(student_score.Key))
+                        {
+                            nextInOrder = student_score;
+                            isSorted = false;
+                        }
+                    }
+                    else
+                    {
+                        if (!studentsSorted.ContainsKey(student_score.Key))
+                        {
+                            nextInOrder = student_score;
+                            isSorted = false;
+                        }
+                    }
+                }
+
+                if (!isSorted)
+                {
+                    studentsSorted.Add(nextInOrder.Key, nextInOrder.Value);
+                    studentsTaken++;
+                    nextInOrder = new KeyValuePair<string, List<int>>();
+                }
+            }
+
+            return studentsSorted;
         }
     }
 }
