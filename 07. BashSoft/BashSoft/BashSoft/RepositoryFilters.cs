64d244977d48c23a971ad5a73bd09da3dbabc430
pirocorp <pirocorp@abv.bg>
2018-05-02 10:01:40 +0200

    Part VI. Functional Programming Part I. Filtered Students Query -Implement Filters

---
+++ 07. BashSoft/BashSoft/BashSoft/RepositoryFilters.cs
@@ -0,0 +1,35 @@
+﻿namespace BashSoft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class RepositoryFilters
+    {
+        public static void FilterAndTake(Dictionary<string, List<int>> wantedData, string wantedFilter, int studentsToTake) 
+        {
+            
+        }
+
+        private static void FilterAndTake(Dictionary<string, List<int>> wantedData, Predicate<double> givenFilter,
+            int studentsToTake)
+        {
+
+        }
+
+        private static bool ExcellentFilter(double mark)
+        {
+            return mark >= 5.0;
+        }
+
+        private static bool AverageFilter(double mark)
+        {
+            return mark < 5.0 && mark >= 3.5;
+        }
+
+        private static bool PoorFilter(double mark)
+        {
+            return mark < 3.5;
+        }
+    }
+}
