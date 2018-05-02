011d115ed42ff99925f125c90f78e3f1d9ef15ca
pirocorp <pirocorp@abv.bg>
2018-05-02 10:34:09 +0200

    Part II: Sorted Students Query Implement Sorters

--- 07. BashSoft/BashSoft/BashSoft/RepositorySorters.cs
+++ 07. BashSoft/BashSoft/BashSoft/RepositorySorters.cs
@@ -6,5 +6,55 @@
 
     public static class RepositorySorters
     {
+        public static void OrderAndTake(Dictionary<string, List<int>> wantedData, string comparison, int studentsToTake)
+        {
+            
+        }
+
+        private static void OrderAndTake(Dictionary<string, List<int>> wantedData, int studentToTake,
+            Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
+        {
+
+        }
+
+        private static int CompareInOrder(KeyValuePair<string, List<int>> firstValue,
+            KeyValuePair<string, List<int>> secondValue)
+        {
+            var totalScoreOfFirst = 0;
+
+            foreach (var mark in firstValue.Value)
+            {
+                totalScoreOfFirst += mark;
+            }
+
+            var totalScoreOfSecond = 0;
+
+            foreach (var mark in secondValue.Value)
+            {
+                totalScoreOfSecond += mark;
+            }
+
+            return totalScoreOfSecond.CompareTo(totalScoreOfFirst);
+        }
+
+        private static int CompareDescendingOrder(KeyValuePair<string, List<int>> firstValue,
+            KeyValuePair<string, List<int>> secondValue)
+        {
+            var totalScoreOfFirst = 0;
+
+            foreach (var mark in firstValue.Value)
+            {
+                totalScoreOfFirst += mark;
+            }
+
+            var totalScoreOfSecond = 0;
+
+            foreach (var mark in secondValue.Value)
+            {
+                totalScoreOfSecond += mark;
+            }
+
+            return totalScoreOfFirst.CompareTo(totalScoreOfSecond);
+        }
     }
 }
