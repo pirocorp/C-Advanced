e9f5fdde8f556e17642a91ac0e63c12c933645ee
pirocorp <pirocorp@abv.bg>
2018-05-03 19:36:06 +0200

    09. Extract Hyperlinks

---
+++ 08. Exam Preparation/09. Extract Hyperlinks/Extract Hyperlinks.cs
@@ -0,0 +1,40 @@
+﻿namespace _09._Extract_Hyperlinks
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+
+    public class ExtractHyperlinks
+    {
+        public static void Main()
+        {
+            var result = new StringBuilder();
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                result.Append(input);
+                result.Append(' ');
+                input = Console.ReadLine();
+            }
+
+            var pattern = @"(?:<a)(?:[\s_0-9a-zA-Z=""()]*?.*?)(?:href([\s]*)?=(?:['""\s]*)?)(?<hyperlinks>[a-zA-Z:#\/._\-0-9!?=^+]*(\([""'a-zA-Z\s.()0-9]*\))?)(?:[\sa-zA-Z=""()0-9]*.*?)?(?:\>)";
+
+            var regex = new Regex(pattern, RegexOptions.Compiled);
+
+            var matches = regex.Matches(result.ToString());
+
+            foreach (Match item in matches)
+            {
+                var value = item.Groups["hyperlinks"].Value;
+
+                if (!value.Contains("fake"))
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+    }
+}
