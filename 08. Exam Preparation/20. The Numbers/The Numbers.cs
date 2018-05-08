76c474054872096f580adb0c8aa0394b27cf66cf
pirocorp <pirocorp@abv.bg>
2018-05-08 18:08:34 +0200

    20. The Numbers

---
+++ 08. Exam Preparation/20. The Numbers/The Numbers.cs
@@ -0,0 +1,43 @@
+﻿namespace _20._The_Numbers
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+
+    public class TheNumbers
+    {
+        public static void Main()
+        {
+            //core version
+            var inputString = Console.ReadLine();
+
+            const string numberPattern = "\\d+";
+
+            var numbers = Regex.Matches(inputString, numberPattern)
+                .Select(x => x.Groups[0].Value)
+                .Select(int.Parse)
+                .ToArray();
+
+            var hexNumbers = numbers.Select(x => "0x" + x.ToString("X4"));
+
+            Console.WriteLine($"{string.Join("-", hexNumbers)}");
+        }
+
+        // .Net Version
+        //            var inputString = Console.ReadLine();
+
+        //            const string numberPattern = "\\d+";
+
+        //            var matches = Regex.Matches(inputString, numberPattern);
+        //            var numbers = new int[matches.Count];
+
+        //            for (var i = 0; i < matches.Count; i++)
+        //            {
+        //                numbers[i] = int.Parse(matches[i].Groups[0].Value);
+        //            }
+
+        //            var hexNumbers = numbers.Select(x => "0x" + x.ToString("X4"));
+
+        //            Console.WriteLine($"{string.Join("-", hexNumbers)}");
+    }
+}
