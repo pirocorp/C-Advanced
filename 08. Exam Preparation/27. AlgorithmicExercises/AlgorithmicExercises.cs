00277b11aeb24fd1a718d175ea822f620f543c55
pirocorp <pirocorp@abv.bg>
2018-05-15 22:26:05 +0200

    Calculator(); ConvertStringtoInteger();

--- 08. Exam Preparation/27. AlgorithmicExercises/AlgorithmicExercises.cs
+++ 08. Exam Preparation/27. AlgorithmicExercises/AlgorithmicExercises.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace _27._AlgorithmicExercises
@@ -8,7 +9,125 @@ namespace _27._AlgorithmicExercises
         public static void Main()
         {
             //ReverseWordsInString();
-            CountConsecutiveDigits();
+            //CountConsecutiveDigits();
+            //Calculator();
+            //ConvertStringtoInteger();
+        }
+
+        private static void ConvertStringtoInteger()
+        {
+            var inputString = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(inputString))
+            {
+                Console.WriteLine($"FormatException");
+                return;
+            }
+
+            var rezult = 0;
+            var multiplayer = 1;
+
+            for (var i = inputString.Length - 1; i >= 0; i--)
+            {
+                var currentItem = inputString[i];
+
+                try
+                {
+                    checked
+                    {
+                        rezult += multiplayer * ParseDigit(currentItem);
+                        multiplayer *= 10;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"OverflowException");
+                    return;
+                }
+            }
+
+            Console.WriteLine(rezult);
+        }
+
+        private static int ParseDigit(char currentItem)
+        {
+            switch (currentItem)
+            {
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                case '0':
+                    return 0;
+                default:
+                    return 0;
+            }
+        }
+
+        private static void Calculator()
+        {
+            var tokens = Console.ReadLine().Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries);
+
+            var expression = new Stack<string>();
+
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                var currentElement = tokens[i];
+
+                if (currentElement.Contains("*") || currentElement.Contains("/"))
+                {
+                    if (currentElement.Contains("*"))
+                    {
+                        var result = decimal.Parse(expression.Pop()) * decimal.Parse(tokens[i + 1]);
+                        expression.Push($"{result}");
+                        i++;
+                    }
+                    else
+                    {
+                        var result = decimal.Parse(expression.Pop()) / decimal.Parse(tokens[i + 1]);
+                        expression.Push($"{result}");
+                        i++;
+                    }
+                }
+                else
+                {
+                    expression.Push(currentElement);
+                }
+            }
+
+            while (expression.Count > 1)
+            {
+                var secondElement = decimal.Parse(expression.Pop());
+                var operand = expression.Pop();
+                var firstElement = decimal.Parse(expression.Pop());
+
+                if (operand == "+")
+                {
+                    var result = firstElement + secondElement;
+                    expression.Push(result.ToString());
+                }
+                else
+                {
+                    var result = firstElement - secondElement;
+                    expression.Push(result.ToString());
+                }
+            }
+
+            Console.WriteLine(expression.Pop());
         }
 
         private static void CountConsecutiveDigits()
