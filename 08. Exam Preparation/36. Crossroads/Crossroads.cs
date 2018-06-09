45c13d41285765f9720719cb3d7b36cbd6d3aae0
pirocorp <pirocorp@abv.bg>
2018-06-09 21:00:55 +0200

    Some Refact

--- 08. Exam Preparation/36. Crossroads/Crossroads.cs
+++ 08. Exam Preparation/36. Crossroads/Crossroads.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace _36._Crossroads
+﻿namespace _36._Crossroads
 {
+    using System;
+    using System.Collections.Generic;
+
     public class Crossroads
     {
         private static int carsPassedCount = 0;
