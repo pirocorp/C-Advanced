cbb16e20dc57c175c88078a62e11b5cf7fb1ef88
pirocorp <pirocorp@abv.bg>
2018-06-09 20:08:05 +0200

    35. Treasure Map

---
+++ 08. Exam Preparation/35. Treasure Map/Treasure Map.cs
@@ -0,0 +1,35 @@
+﻿namespace _35._Treasure_Map
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class TreasureMap
+    {
+       private const string ValidateInstructionPattern = "(!|#)[^!#]*?\\b(?<street>[A-Za-z]{4})\\b[^!#]*(?<!\\d)(?<number>\\d{3})-(?<password>\\d{6}|\\d{4})(?!\\d)[^!#]*?(?!\\1)(#|!)";
+
+        public static void Main()
+        {
+            var instructionsRegex = new Regex(ValidateInstructionPattern, RegexOptions.Compiled);
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine();
+                var matches = instructionsRegex.Matches(inputLine);
+                var currentInstructionIndex = matches.Count / 2;
+                var currentInstruction = matches[currentInstructionIndex];
+                PrintDataFromInstruction(currentInstruction);
+            }
+        }
+
+        private static void PrintDataFromInstruction(Match match)
+        {
+            var streetName = match.Groups["street"].Value;
+            var streetNumber = match.Groups["number"].Value;
+            var password = match.Groups["password"].Value;
+            Console.WriteLine($"Go to str. {streetName} {streetNumber}. Secret pass: {password}.");
+        }
+    }
+}
+    
