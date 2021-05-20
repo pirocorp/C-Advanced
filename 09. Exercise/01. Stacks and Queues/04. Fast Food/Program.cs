b4bb4e01453961ea81f0972ee70e6690c986fbc4
Zdravko Zdravkov <zdr0@yahoo.com>
2021-05-20 17:51:10 +0200

    01. Stacks and Queues

---
+++ 09. Exercise/01. Stacks and Queues/04. Fast Food/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _04._Fast_Food
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var queue = new Queue<int>();
+            var food = int.Parse(Console.ReadLine());
+
+            ProcessInput(queue);
+
+            var max = queue.Max(x => x);
+
+            while (queue.Any())
+            {
+                if (queue.Peek() <= food)
+                {
+                    food -= queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(max);
+
+            if (queue.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+
+        private static void ProcessInput(Queue<int> queue)
+        {
+            var orders = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            foreach (var order in orders)
+            {
+                queue.Enqueue(order);
+            }
+        }
+    }
+}
---
+++ 09. Exercise/01. Stacks and Queues/07. Truck Tour/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _07._Truck_Tour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<int[]>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var pump = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                queue.Enqueue(pump);
+            }
+
+            for (var currentStart = 0; currentStart < n; currentStart++)
+            {
+                var fuel = 0;
+                var fullCircle = true;
+
+                for (var distance = 0; distance < n; distance++)
+                {
+                    var currentPump = queue.Dequeue();
+                    queue.Enqueue(currentPump);
+
+                    var pumpFuel = currentPump[0];
+                    var nextPumpDistance = currentPump[1];
+
+                    fuel += pumpFuel - nextPumpDistance;
+
+                    if (fuel < 0)
+                    {
+                        currentStart += distance;
+                        fullCircle = false;
+                        break;
+                    }
+                }
+
+                if (fullCircle)
+                {
+                    Console.WriteLine(currentStart);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
