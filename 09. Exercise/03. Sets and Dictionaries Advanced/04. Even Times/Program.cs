d9dd6d74c2caf423c26bb0d8fe8d7f8edba2878e
Zdravko Zdravkov <zdr0@yahoo.com>
2021-05-21 17:13:33 +0200

    03. Sets and Dictionaries Advanced

---
+++ 09. Exercise/03. Sets and Dictionaries Advanced/04. Even Times/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04._Even_Times
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var collection = new Dictionary<int, int>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+
+                if (!collection.ContainsKey(input))
+                {
+                    collection[input] = 0;
+                }
+
+                collection[input]++;
+            }
+
+            var result = collection.First(x => x.Value % 2 == 0).Key;
+            Console.WriteLine(result);
+        }
+    }
+}
